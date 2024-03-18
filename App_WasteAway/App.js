f3f211daedd64fc9b420ab989598050a721be440
Peu <pedrocavalcanti.it@gmail.com>
2024-03-18 18:51:14 -0300

    Tela login finalizada

--- App_WasteAway/App.js
+++ App_WasteAway/App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { Home } from './src/screens/Home';
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
     <>
+      <StatusBar translucent={true}/>
+      <View style={styles.containerFather}>
+
       <View style={styles.header}>
         <Text style={styles.title}>Login</Text>
       </View>
@@ -12,20 +15,36 @@ export default function App() {
       <View style={styles.container}>
 
         <View style={styles.mainContent}>
-          
-          <View style={styles.inputsBox}>
-          <Text style={styles.inputTitle}>E-mail</Text>
-          <TextInput style={styles.placeHolder} placeholder='Digite seu e-mail...' placeholderTextColor="#FFF"></TextInput>
 
-          <Text style={styles.inputTitle}>Senha</Text>
-          <TextInput style={styles.placeHolder} placeholder='Digite sua senha...' placeholderTextColor="#FFF"></TextInput>
+          <View style={styles.inputsBox}>
+            <Text style={styles.inputTitle}>E-mail</Text>
+            <TextInput style={styles.placeHolder} placeholder='Digite seu e-mail...' placeholderTextColor="#FFF"></TextInput>
+
+            <Text style={styles.inputTitle}>Senha</Text>
+            <TextInput style={styles.placeHolder} placeholder='Digite sua senha...' placeholderTextColor="#FFF"></TextInput>
           </View>
+
+          <Text style={styles.forgotPassword}>Esqueceu a senha?</Text>
 
           <TouchableOpacity style={styles.button}>
             <Text style={styles.buttonText}>Acessar</Text>
           </TouchableOpacity>
 
+          <Text style={styles.textDecoration}>OU</Text>
+          <Text style={styles.textDecoration}>Login com</Text>
+
+          <View style={styles.loginRedes}>
+            <View>
+              <Image source={require('./src/assets/Google.png')} />
+            </View>
+            <View style={styles.facebookImg}>
+              <Image source={require('./src/assets/Facebook.png')} />
+            </View>
+          </View>
+
         </View>
+
+      </View>
 
       </View>
 
@@ -34,29 +53,36 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
+  containerFather: {
+    backgroundColor: "#FA5F49",
+    paddingBottom: 40,
+  },
   header: {
     alignItems: "center",
   },
   title: {
-    marginVertical: 50,
+    marginTop: 60,
+    marginBottom: 40,
     fontSize: 30,
+    color: '#FFF',
+    fontWeight: '700',
   },
   container: {
-    display: 'flex',
     alignItems: 'center',
 
   },
   mainContent: {
     width: 350,
     padding: 20,
-    backgroundColor: '#ccc',
+    backgroundColor: '#FFF',
     borderRadius: 10,
-    display: 'flex',
     alignItems: 'center',
+
   },
   inputTitle: {
     fontSize: 18,
     width: 300,
+    marginTop: 50
 
   },
   placeHolder: {
@@ -65,17 +91,38 @@ const styles = StyleSheet.create({
     borderRadius: 50,
     padding: 16,
     color: '#fff',
-    marginBottom: 30,
   },
-  button:{
+  forgotPassword: {
+    textDecorationLine: 'underline',
+    fontSize: 14,
+    color: '#44436C',
+    marginLeft: 150,
+    marginBottom: 50,
+
+  },
+  button: {
     backgroundColor: '#FA5F49',
     alignItems: 'center',
     width: 150,
     padding: 10,
     borderRadius: 50,
+    marginBottom: 30
   },
   buttonText: {
     color: '#FFF',
+  },
+  loginRedes: {
+    flexDirection: 'row',
+    marginTop: 40,
+    marginBottom: 40,
+  },
+  textDecoration: {
+    color: '#8C8CB0',
+    marginTop: 10,
+    fontWeight: '800'
+  },
+  facebookImg: {
+    marginLeft: 30,
   }
 })
 
