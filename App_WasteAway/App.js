9f4516b2f52a01f69aa6ff99e10516c239049e88
Peu <wasteaway.equipe@gmail.com>
2024-03-18 10:59:46 -0300

    Código base pronto

--- App_WasteAway/App.js
+++ App_WasteAway/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>WasteAway - Início</Text>
       <StatusBar style="auto" />
     </View>
   );
